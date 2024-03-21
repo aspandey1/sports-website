1a91b4b0ffaa5ebd6af438ae586403a57b247d78
Ashish Pandey <ashish.apandey@hotmail.com>
2024-03-21 16:30:42 -0500

    Testing getChampGoal function with azure functions

--- frontend/src/services/requests/ChampLeague.ts
+++ frontend/src/services/requests/ChampLeague.ts
@@ -2,18 +2,15 @@ import axios from "axios";
 
 export async function getChampLeagueData() {
   let local = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const response = await axios.get(
-    "https://sports-website-backend.azurewebsites.net/api/httpTriggerGetChampGoals?",
-    {
-      params: { local },
-    }
-  );
+  const response = await axios.get("", {
+    params: { local },
+  });
   return response.data;
 }
 
 export async function getChampGoalData() {
   const response = await axios.get(
-    "http://localhost:7071/api/httpTriggerGetChampGoals"
+    "https://sports-website-backend.azurewebsites.net/api/httpTriggerGetChampGoals?"
   );
   return response.data;
 }
