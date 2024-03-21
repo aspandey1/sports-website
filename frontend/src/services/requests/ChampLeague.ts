704ef924fde8eedac515f4ff7c7000e68694873b
Ashish Pandey <ashish.apandey@hotmail.com>
2024-03-21 16:21:06 -0500

    Testing azure serverless functions with getChampGoals request

--- frontend/src/services/requests/ChampLeague.ts
+++ frontend/src/services/requests/ChampLeague.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function getChampLeagueData() {
   let local = Intl.DateTimeFormat().resolvedOptions().timeZone;
   const response = await axios.get(
-    "http://localhost:7071/api/httpTriggerGetChampMatches",
+    "https://sports-website-backend.azurewebsites.net/api/httpTriggerGetChampGoals?",
     {
       params: { local },
     }
