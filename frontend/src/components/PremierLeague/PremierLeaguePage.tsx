87b69c2dec686b0ef87a658d3f26cde6302bc770
Ashish Pandey <ashish.apandey@hotmail.com>
2024-03-30 23:56:49 -0500

    Uncommented backend endpoints

--- frontend/src/components/PremierLeague/PremierLeaguePage.tsx
+++ frontend/src/components/PremierLeague/PremierLeaguePage.tsx
@@ -30,29 +30,29 @@ const PremierLeaguePage = () => {
     window.scrollTo(0, 0);
   }, []);
 
-  // useEffect(() => {
-  //   getPremMatches()
-  //     .then((data) => {
-  //       if (data.matches.length == 0) {
-  //         setPremMatchesData(tempMatchValue);
-  //       } else setPremMatchesData(data.matches);
-  //     })
-  //     .catch(() => {
-  //       setPremMatchesData(tempMatchValue);
-  //     });
-  // }, []);
+  useEffect(() => {
+    getPremMatches()
+      .then((data) => {
+        if (data.matches.length == 0) {
+          setPremMatchesData(tempMatchValue);
+        } else setPremMatchesData(data.matches);
+      })
+      .catch(() => {
+        setPremMatchesData(tempMatchValue);
+      });
+  }, []);
 
-  // useEffect(() => {
-  //   getPremStandings()
-  //     .then((data) => {
-  //       if (data.standings.length === 0) {
-  //         setPremStandingsData(tempStandingsValue);
-  //       } else setPremStandingsData(data.standings);
-  //     })
-  //     .catch(() => {
-  //       setPremStandingsData(tempStandingsValue);
-  //     });
-  // }, []);
+  useEffect(() => {
+    getPremStandings()
+      .then((data) => {
+        if (data.standings.length === 0) {
+          setPremStandingsData(tempStandingsValue);
+        } else setPremStandingsData(data.standings);
+      })
+      .catch(() => {
+        setPremStandingsData(tempStandingsValue);
+      });
+  }, []);
 
   return (
     <>
